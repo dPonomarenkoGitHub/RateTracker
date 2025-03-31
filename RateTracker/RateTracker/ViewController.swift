6e0a3269e7452b4dbf897e2ff2e5d03851dc8df1
Dmitry Ponomarenko <dmitry@zerion.io>
2025-03-31 16:13:38 +0300

    initial

---
+++ RateTracker/RateTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RateTracker
+//
+//  Created by Dmitry Ponomarenko on 31.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
